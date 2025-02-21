8b6d7ab3e6592c926aad5947ab6d5bdd313a7d33
Almeida1234 <almeidasilva_rick@hotmail.com>
2025-02-21 14:25:15 -0300

    Exercícios do curso de Java

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,37 @@
 package fundamentos;
 
+/**
+ * Essa Classe representa ... JAVADOC
+ * 
+ * @author ricardo
+ * @since JDK1.0
+ * @see
+ */
+
 public class PrimeiroPrograma {
+	// digitando o " /** " cria um comentadio do Java Doc
+	/**
+	 * 
+	 * @param args
+	 * @
+	 */
 	public static void main(String[] args) {
+		
+		// Uma sentença de código termina com ; 
+		// Mais um comentário...
+		// Fim
 		System.out.println("Primeiro Programa Parte #01");
+		
+		/*
+		 
+		 Linha 1
+		 Linha 2
+		  
+		"O crtl + / " comenta a linha de Código 
+		 */
 		System.out.println("Primeiro Programa Parte #02");
+		
+		System.out.println("Fim!!!"); // Aqui também funciona!
 	}
 	
 }
