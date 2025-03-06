906e083a53c842137f23b17079760530b772a021
Almeida1234 <almeidasilva_rick@hotmail.com>
2025-03-06 20:35:42 -0300

    Wrapper

---
+++ exercicios/src/fundamentos/Wrapper.java
@@ -0,0 +1,44 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite um número: ");
+
+		// Para os demais Byte, Short e Long escrevendo com a letra minuscula ele já
+		// vira um primitivo.
+		// byte
+		Byte b = 100;
+		Short s = 1000;
+
+		Integer j = Integer.parseInt(entrada.next());
+		Integer i = 10000; // Tipo primitivo é o int
+		Long l = 100000L;
+
+		System.out.println(b.byteValue());
+		System.out.println(s.toString());
+		System.out.println(j * 3);
+		System.out.println(i * 3);
+		System.out.println(l /3);
+		
+		Float f = 123.11F;
+		System.out.println(f);
+		
+		Double d = 1234.5678;
+		System.out.println(d);
+		
+		Boolean bo = Boolean.parseBoolean("true");
+		System.out.println(bo);
+		System.out.println(bo.toString().toUpperCase());
+		
+		Character c = '#'; // char
+		System.out.println(c + "...");
+
+		entrada.close();
+	}
+
+}
