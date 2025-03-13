84ffee18599c77917b3c2e1db681e4bfc7285dc4
Almeida1234 <almeidasilva_rick@hotmail.com>
2025-03-13 10:02:25 -0300

    Ternario

--- exercicios/src/fundamentos/operadores/Ternario.java
+++ exercicios/src/fundamentos/operadores/Ternario.java
@@ -4,19 +4,28 @@ public class Ternario {
 	
 	public static void main(String[] args) {
 		
-		double media = 8.6;		
+		double media = 7.6;
+		String resultadoParcial = media >= 5.0 ?
+				"recuperação." : "reprovado.";
 		String resultadoFinal = media >= 7.0 ? 
-				"aprovado." : "em recuperação.";
+				"aprovado." : resultadoParcial;
 		System.out.println("O aluno está " + resultadoFinal);
 		
+		boolean bomComportamento = true;
+		boolean passouPorMedia = media >= 7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		String resultado = temDesconto ? "Sim." : "Não.";
 		
+		System.out.printf("Tem desconto? %s", resultado);
+		/*
 		double nota = 9.9;
 		boolean bomComportamento = true;
 		boolean passouPorMedia = nota >= 7;
 		boolean temDesconto = bomComportamento && passouPorMedia;
 		String resultado = temDesconto ? "Sim." : "Não.";
-		
+				
 		System.out.printf("Tem desconto? %s", resultado);
+		*/
 	}
 
 }
