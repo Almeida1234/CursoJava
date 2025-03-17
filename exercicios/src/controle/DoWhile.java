d2cddd854698f3e84064b93795adf04595e67f2b
Almeida1234 <almeidasilva_rick@hotmail.com>
2025-03-17 14:07:11 -0300

    do While

--- exercicios/src/controle/DoWhile.java
+++ exercicios/src/controle/DoWhile.java
@@ -11,8 +11,7 @@ public class DoWhile {
 		String texto = "";
 		
 		do {
-			System.out.println("Você precisa falar "
-					+ "\nas palavras mágicas...");
+			System.out.println("Você precisa falar as palavras mágicas...");
 			System.out.print("Quer sair? ");
 			texto = entrada.nextLine();
 		} while(!texto.equalsIgnoreCase("por favor"));
