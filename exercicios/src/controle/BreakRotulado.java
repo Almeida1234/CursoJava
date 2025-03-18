570d92ed6c2f28bc624db5bf65910143cddaff0b
Almeida1234 <almeidasilva_rick@hotmail.com>
2025-03-18 16:00:15 -0300

    Break Rotulado

--- exercicios/src/controle/BreakRotulado.java
+++ exercicios/src/controle/BreakRotulado.java
@@ -4,11 +4,11 @@ public class BreakRotulado {
 	
 	public static void main(String[] args) {
 		
-		externo:
+		//externo:
 			for (int i = 0; i < 3; i++) {
 				for (int j = 0; j < 3; j++) {
 					if(i == 1) {
-						break externo;
+						break; //externo;
 					}
 					System.out.printf("[%d %d] ", i, j);
 				}
