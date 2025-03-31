2b0b8a5131e116826b1f873fadba5bed619dc5fd
Almeida1234 <almeidasilva_rick@hotmail.com>
2025-03-31 10:29:37 -0300

    Conversão Numero para String

--- exercicios/src/fundamentos/ConversaoNumeroString.java
+++ exercicios/src/fundamentos/ConversaoNumeroString.java
@@ -9,7 +9,7 @@ public class ConversaoNumeroString {
 		int num2 = 100000;
 		System.out.println(Integer.toString(num2).length());
 		
-		System.out.println(("" + num1).length());
+		System.out.println(("" + num1));
 		System.out.println(("" + num2).length());
 	}
 	
