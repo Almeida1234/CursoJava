72fd0b762925bd41eb1ff3492eb076c7e99f0c94
Almeida1234 <almeidasilva_rick@hotmail.com>
2025-05-05 16:54:23 -0300

    Orientação a Objeto - Polimorfismo

---
+++ exercicios/src/orientacaoObjeto/polimorfismo/Feijao.java
@@ -0,0 +1,9 @@
+package orientacaoObjeto.polimorfismo;
+
+public class Feijao extends Comida{
+	
+	public Feijao(double peso) {
+		super(peso);
+	}
+
+}
---
+++ exercicios/src/orientacaoObjeto/polimorfismo/Jantar.java
@@ -0,0 +1,26 @@
+package orientacaoObjeto.polimorfismo;
+
+public class Jantar {
+	
+	public static void main(String[] args) {
+		
+		Pessoa convidado = new Pessoa(99.65);
+		
+		Arroz igrediente1 = new Arroz(0.2);
+		Feijao igrediente2 = new Feijao(0.1);
+		
+		System.out.println(convidado.getPeso());
+		
+		convidado.Comer(igrediente1);
+		convidado.Comer(igrediente2);
+	
+		System.out.println(convidado.getPeso());
+	
+		Sorvete sobremesa = new Sorvete(0.4);
+		convidado.Comer(sobremesa);
+		
+		System.out.println(convidado.getPeso());
+
+	}
+
+}
